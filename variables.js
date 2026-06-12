bd52520b8680f0dd493dec0e2b214e53fcb6fe51
imdee47 <deepikag25csa@vetias.ac.in>
2026-06-12 10:34:43 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+//variable declaration using var
+var a='30'
+console.log(a)
+// variable declaration using let
+let name='deepika'
+console.log(name)
+//variable using const
+const country = "india"
+console.log(country)
