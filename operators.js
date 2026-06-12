4fef216d6bbac7d0e63958be2fc19dbe4810215a
imdee47 <deepikag25csa@vetias.ac.in>
2026-06-12 11:55:16 +0530

    comparison

--- operators.js
+++ operators.js
@@ -1,8 +1,38 @@
-//Arthimetic
-var a=40
-var b=70
-console.log(a+b)//addition
-console.log(a-b)//subraction
-console.log(a*b)//product
-console.log(a%b)//mod
-console.log(a**b)//expo
+// Arithmetic Operator 
+
+var a = 10 
+var b = 20 
+
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+
+var c = 10 
+
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+
+
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+console.log(a+b);
+
+
+//Comparison Operator
+console.log("Comparison Operator")
+var a=10
+var b=40
+console.log(a==b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
