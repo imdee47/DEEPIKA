bdcfaaacb5590b4cfe7ae93a2c32cb73bb74ffdc
imdee47 <deepikag25csa@vetias.ac.in>
2026-06-12 13:59:28 +0530

    Unary

--- operators.js
+++ operators.js
@@ -43,3 +43,21 @@ console.log(a > 0 && b > 0); // o/p - true
 console.log(a > 0 && b < 0); // o/p - false
 console.log(a < 0 || b < 0); // o/p - false
 console.log(!(a > b)); // o/p -false
+
+
+//Assignment
+console.log("Assignment Operator")
+var c=10
+console.log(c+=30)
+console.log(c-=30)
+console.log(c)
+console.log(c)
+console.log(c)
+
+//Unary Operator
+console.log("Unary Operator")
+
+console.log(c++);// 30 c - 31
+console.log(c--);// 31 c - 30
+console.log(--c);// 29 c - 29
+console.log(++c);// 30 c - 30
