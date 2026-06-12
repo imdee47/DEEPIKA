7d8e8234121bcb564d44070a1549c1a04d247bd4
imdee47 <deepikag25csa@vetias.ac.in>
2026-06-12 15:53:42 +0530

    conditional

---
+++ conditional.js
@@ -0,0 +1,12 @@
+var light = "white"
+
+if(light=="green"){
+    console.log("go!");
+}
+else if(light == "yellow"){
+    console.log("wait!")
+}
+else {
+    console.log("invalid light color");
+}
+console.log("out side if");
---
+++ funs.js
@@ -0,0 +1,14 @@
+//Non parametric Functions
+function greet(fav actor) {
+   console.log( "favourite Actor : Dhruv Vikram");
+
+}
+greet(); // output: Hello, world!
+
+//parametric functions
+functin add(a,b){
+
+    return a + b;
+
+}
+console.log(add(5,3));
